78f5eaa9226d3409e66cbb00b9cb72ca30e1b08b
parshantY <parshant648@gmail.com>
2022-06-07 22:20:40 +0530

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,76 @@
+const cityName = document.getElementById('cityName');
+const submitBtn = document.getElementById('submitBtn');
+
+const city_name = document.getElementById('city_name');
+
+const temp_real_val = document.getElementById('temp_real_val');
+const temp_status = document.getElementById('temp_status');
+
+const datahide = document.querySelector('.middle_layer');
+console.log("trace")
+
+const getInfo = async(event) => {
+	event.preventDefault();
+	let cityVal = cityName.value;
+	if (cityVal === "") {
+		city_name.innerText = `Plz write the name before search`;
+		datahide.classList.add('data_hide');
+      
+
+	} else {
+		try {
+			let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&appid=d0cafc971750c2b87a05ac4c5529fd6e&units=metric`;
+			const response = await fetch(url);
+			const data = await response.json();
+			const arrData = [data];
+			console.log(arrData)
+			city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
+			temp_real_val.innerText =(arrData[0].main.temp);
+
+			const tempMood = arrData[0].weather[0].main;
+
+			//condition to check sunny or cloudy
+			if (tempMood == "Clear") {
+				temp_status.innerHTML =
+					"<i class='fas  fa-sun' style='color: #eccc68;'></i>";
+			} else if (tempMood == "Clouds") {
+				temp_status.innerHTML =
+					"<i class='fas  fa-cloud' style='color: #f1f2f6;'></i>";
+			} else if (tempMood == "Rain") {
+				temp_status.innerHTML =
+					"<i class='fas  fa-cloud-rain' style='color: #a4b0be;'></i>";
+			} else {
+				temp_status.innerHTML =
+					"<i class='fas  fa-sun' style='color:#eccc68;'></i>";
+			}
+
+			datahide.classList.remove('data_hide');
+
+		} catch {
+			city_name.innerText = `Plz enter the city name properly`;
+			datahide.classList.add('data_hide');
+		}
+	}
+
+}
+submitBtn.addEventListener('click', getInfo);
+console.log("aaaaaa")
+const getCurrentDay = () => {
+	let weekday = new Array(7);
+	weekday[0] = "Sunday";
+	weekday[1] = "Monday";
+	weekday[2] = "Tuesday";
+	weekday[3] = "Wed";
+	weekday[4] = "Thursday";
+	weekday[5] = "Friday";
+	weekday[6] = "Saturday";
+
+	let currentTime = new Date();
+	days = weekday[currentTime.getDay()];
+	let day = document.getElementById('day');
+	let date = document.getElementById("today_data");
+    date.innerText = currentTime.toLocaleString();
+	day.innerText = days;
+};
+
+getCurrentDay();
